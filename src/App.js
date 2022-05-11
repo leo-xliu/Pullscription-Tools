f46a9aa2eb75ce38ea204607aaec51310a797336
unknown <leoxiyuliu@gmail.com>
2022-05-11 03:35:13 -0700

    Added react-router and app is now usable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
+    <div>
+      <h1> This should show the home page</h1>
+      {/*header*/}
+      {/*homepage*/}
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Login from './Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-);
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App/>} />
+            <Route path="/login" element={<Login/>} />
+        </Routes>
+    </BrowserRouter>
+); 
