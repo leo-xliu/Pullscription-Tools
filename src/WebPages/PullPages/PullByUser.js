61f862d45c5766a358db3670a814a3219d7738f6
SteveZ <steve.zang007@gmail.com>
2022-05-29 15:36:55 -0700

    renew PulledComicsProcess

--- src/WebPages/PullPages/PullByUser.js
+++ src/WebPages/PullPages/PullByUser.js
@@ -2,46 +2,90 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import {useState} from "react";
 import CustomerProfileBase from '../../Components/CustomerProfileBase';
+import  Pagination  from '../../Components/PaginationFeature/Pagination'
+import Header from '../../Components/Header';
+import PulledComicsProcess from './PulledComicsProcess';
 
-export default function PullByUser() {
+export default function PullByUser(props, arg, props2) {
 
     const [query, setQuery] = useState("")
+    const [currentPage, setCurrentPage] = useState(1)
+    const [fanPerPage, setFanPerPage] = useState(10)
+
+
+    var customerProfile = CustomerProfileBase.filter(data => {
+      let fullName = data.firstName + " "+ data.lastName
+      if (query === '') {
+        return data;
+      } else if ((fullName).toLowerCase().includes(query.toLowerCase())) {
+        return data;
+      }
+    }).map((data, index) => (
+      <div className="box" key={index}>
+        <p>{data.firstName} {data.lastName}</p>
+        <Link to="/PullComics/PullByUser/PulledComics"
+            state={{
+                PULLEDCOMICS: data.pulledComics,
+                PULLEDCOMICSSETASIDE: data.pulledComicsSetAside
+            }}
+        >
+            <img src={data.IMAGE_URL_SMALL} alt="Logo" />
+        </Link>
+      </div>
+    ))
+
+
+    var newProfile = customerProfile
+    const indexOfLastFan = currentPage * fanPerPage
+    const indexOfFirstFan = indexOfLastFan - fanPerPage
+    const currentFan = newProfile.slice(indexOfFirstFan, indexOfLastFan)
 
     return (
         <div className="box">
+          <Header/>
           <h1>Pull By User!!</h1>
           {/* <li><Link to="/PullComics">Pull Comics</Link></li>
           <li><Link to="/">Main</Link></li> */}
         <input placeholder="Enter Comic Title" onChange={event => setQuery(event.target.value)} />
         {
-          //{dataComponents}  
-            CustomerProfileBase.filter(data => {
-              let fullName = data.firstName + " "+ data.lastName
-              if (query === '') {
-                return data;
-              } else if ((fullName).toLowerCase().includes(query.toLowerCase())) {
-                return data;
-              }
-            }).map((data, index) => (
-              <div className="box" key={index}>
-                <p>{data.firstName} {data.lastName}</p>
-                {/*<p>{data.WRITER}</p>*/}
-                    {/* <BrowserRouter>
-                        <Routes>
-                            <Route path="/Inventory/ComicProfile" element={<ComicProfile props={data}/>} />
-                        </Routes>
-                    </BrowserRouter> */}
-                <Link to="/PullComics/PullByUser/PulledComics"
-                    state={{
-                        PULLEDCOMICS: data.pulledComics
-                    }}
-                >
-                    <img src={data.IMAGE_URL_SMALL} alt="Logo" />
-                </Link>
-              </div>
-            ))
+          <div>{customerProfile}</div>
         }
+        <Pagination 
+                /*comicsPerPage={comicsPerPage}
+                totalComics={inventoryComics.length}
+                paginate = {paginate}*/
+                currentPage={currentPage}
+                totalSize={customerProfile.length}
+                changeCurrentPage={setCurrentPage}
+                theme="bootstrap"
+          />
     </div>
     )
 }
 
+
+// CustomerProfileBase.filter(data => {
+//   let fullName = data.firstName + " "+ data.lastName
+//   if (query === '') {
+//     return data;
+//   } else if ((fullName).toLowerCase().includes(query.toLowerCase())) {
+//     return data;
+//   }
+// }).map((data, index) => (
+//   <div className="box" key={index}>
+//     <p>{data.firstName} {data.lastName}</p>
+//     {/*<p>{data.WRITER}</p>*/}
+//         {/* <BrowserRouter>
+//             <Routes>
+//                 <Route path="/Inventory/ComicProfile" element={<ComicProfile props={data}/>} />
+//             </Routes>
+//         </BrowserRouter> */}
+//     <Link to="/PullComics/PullByUser/PulledComics"
+//         state={{
+//             PULLEDCOMICS: data.pulledComics
+//         }}
+//     >
+//         <img src={data.IMAGE_URL_SMALL} alt="Logo" />
+//     </Link>
+//   </div>
+// ))
---
+++ src/WebPages/PullPages/PulledComicsProcess.js
@@ -0,0 +1,62 @@
+
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import {useState, useEffect} from "react";
+import Header from '../../Components/Header';
+import DataBase from '../../Components/DataBase';
+import '../Inventory.css'
+import { AddOutlined } from '@mui/icons-material';
+
+export default function PulledComicsProcess() {
+
+    const[NUM, SETNUM] = useState(0)
+
+    const location = useLocation()
+    const id = location.state.PS_NO
+    const MAIN_DESC = location.state.MAIN_DESC
+    const PUBLISHER = location.state.PUBLISHER
+    const IMAGE_URL_SMALL = location.state.IMAGE_URL_SMALL
+    const EXPECTED_PULL = location.state.EXPECTED_PULL
+    const NUM_COPY = location.state.NUM_COPY
+
+    const constNum = NUM_COPY
+
+    //console.log("Current_Comics:"+CURRENT_COMICS)
+
+    const addOne = () => {
+        SETNUM(NUM+1)
+    }
+
+    const minusOne = () => {
+        SETNUM(NUM-1)
+    }
+
+    const addFive = () => {
+        SETNUM(NUM+5)
+    }
+
+    const minusFive = () => {
+        SETNUM(NUM-5)
+    }
+
+    return (
+        <div>
+            <Header/>
+            <div>
+                <img className="comic-cover" src={IMAGE_URL_SMALL}></img>
+                <div className="comic-title"> 
+                     <p>{MAIN_DESC}</p>
+                     <h6>{PUBLISHER}</h6>
+                </div>
+                <button onClick={addFive}>+5</button>
+                <button onClick={addOne}>+1</button>
+                <button onClick={minusOne}>-1</button>
+                <button onClick={minusFive}>-5</button>
+            </div>
+            <Link to='/PullComics/PullByUser'>Confirm</Link>
+            {/* <li><Link to="/PullComics/PullByWeek">Pull_By_Week</Link></li>
+            <li><Link to="/PullComics/PullByUser">Pull_By_User</Link></li>
+            <li><Link to="/">Main</Link></li> */}
+        </div>
+    )
+}
