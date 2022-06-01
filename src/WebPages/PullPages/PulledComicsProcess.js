b70efd84ba0c8e71742f78d5512b338d2e21b030
Leo Liu <leoxiyuliu@gmail.com>
2022-05-31 21:38:41 -0700

    more css complete

--- src/WebPages/PullPages/PulledComicsProcess.js
+++ src/WebPages/PullPages/PulledComicsProcess.js
@@ -4,10 +4,10 @@ import { Link, useLocation } from 'react-router-dom';
 import {useState, useEffect} from "react";
 import Header from '../../Components/Header';
 import DataBase from '../../Components/DataBase';
-import '../Inventory.css'
 import CustomerProfileBase from '../../Components/CustomerProfileBase';
 import searchFanProfile from '../../Components/searchFanProfile';
 import searchID from '../../Components/searchID';
+import './PulledComicsProcess.css';
 
 export default function PulledComicsProcess() {
 
@@ -61,23 +61,33 @@ export default function PulledComicsProcess() {
     return (
         <div>
             <Header/>
-            <div>
-                <img className="comic-cover" src={IMAGE_URL_SMALL}></img>
-                <div className="comic-title"> 
-                    {/* {console.log("searchID: "+searchID(P))} */}
-                     <p>{MAIN_DESC}</p>
-                     <h6>{PUBLISHER}</h6>
+            <div className="user-comics-pull">
+                <div className="user-comics-panel">
+                    <img className="user-comics-cover" src={IMAGE_URL_SMALL}></img>
+                    <div className="user-comics-info"> 
+                        {/* {console.log("searchID: "+searchID(P))} */}
+                        <p>{MAIN_DESC}</p>
+                        <h6>{PUBLISHER}</h6>
+                    </div>
                 </div>
-                <button onClick={addFive}>+5</button>
-                <button onClick={addOne}>+1</button>
-                <div style={{ color: "green" }}>{NUM}</div>
-                <button onClick={minusOne}>-1</button>
-                <button onClick={minusFive}>-5</button>
+                <div className="function-pad">
+                    <button className="function-pad-button" onClick={addFive}>+5</button>
+                    <button className="function-pad-button" onClick={addOne}>+</button>
+                    <div className="total-num">{NUM}</div>
+                    <button className="function-pad-button" onClick={minusOne}>-</button>
+                    <button className="function-pad-button" onClick={minusFive}>-5</button>
+                </div>
+            <div className="confirm-button-box">
+                 <Link className="confirm-button" to='/PullComics/PullByUser'>CONFIRM</Link>
             </div>
-            <Link to='/PullComics/PullByUser'>Confirm</Link>
             {/* <li><Link to="/PullComics/PullByWeek">Pull_By_Week</Link></li>
             <li><Link to="/PullComics/PullByUser">Pull_By_User</Link></li>
             <li><Link to="/">Main</Link></li> */}
+
+                <div className="return-button">
+                    <Link className="return-button-link" to="/PullComics/PullByUser">Return</Link>
+                </div>
+            </div>
         </div>
     )
 }
